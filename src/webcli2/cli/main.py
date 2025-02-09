e7af36568dbc3e33bbe508e6213e49b4d68a8a94
Stone Zhong <shidong.zhong@oracle.com>
2025-02-09 07:07:03 -0800

    update

--- src/webcli2/cli/main.py
+++ src/webcli2/cli/main.py
@@ -1,146 +1,43 @@
+from typing import Optional
 import os
+import logging.config
 import yaml
-import logging
-import logging.config        
-import uvicorn
-import argparse
-from sqlalchemy import create_engine
-from webcli2.db_models import create_all_tables
-import getpass
+from webcli2.config import load_config, WebCLIApplicationConfig, normalize_filename
 
-from webcli2.config import WebCLIApplicationConfig, load_config, normalize_filename
-from webcli2.webcli_engine import UserManager
-
-logger = None
-
-class WebCLIApplication:
-    webcli_home:str                     # WebCLI application home directory
-    config_filename:str                 # WebCLI config file name, it is a YAML file
-    config:WebCLIApplicationConfig      # config loaded from webcli_cfg.yaml
-    
-    def initialize_logging(self):
-        # create loging directory
-        os.makedirs(self.config.core.log_dir, exist_ok=True)
-
-        # loading logging config
-        if not os.path.isfile(self.config.core.log_config_filename):
-            print(f"Missing log config file: {self.config.core.log_config_filename}")
-            exit(1)
-        with open(self.config.core.log_config_filename, "rt") as f:
-            global logger
-            log_config = yaml.safe_load(f)
-        # patch filename field
-        for _, handler_config in log_config.get("handlers", {}).items():
-            if "filename" in handler_config:
-                handler_config["filename"] = normalize_filename(
-                    self.config.core.log_dir, 
-                    handler_config["filename"]
-                )
-        logging.config.dictConfig(log_config)
-        
-        logger = logging.getLogger(__name__)
-        logger.info("Logging is initialized")
-
-
-    def initialize(self):
-        self.config = load_config()
-        if self.config is None:
-            print(f"Missing config file")
-            exit(1)
-
-        self.initialize_logging()
-        
-
-    def start(self):
-        self.initialize()
-        from webcli2.web import app
-        # run application
-        uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
+#############################################################
+# This module do the following
+# - load config
+# - initialize logging
+# then pass contrl to webcli_internal
+#############################################################
 
 def webcli():
-    parser = argparse.ArgumentParser(
-        description='WebCLI Tool.'
-    )
-    parser.add_argument(
-        "action", type=str, help="Specify action",
-        choices=['start', 'init-db', 'create-user'],
-        nargs=1
-    )
-    parser.add_argument(
-        "--email", type=str, required=False, help="user email"
-    )
-    args = parser.parse_args()
-    action = args.action[0]
-    if action == "start":
-        webcli_application = WebCLIApplication()
-        webcli_application.start()
-        return
+    config:Optional[WebCLIApplicationConfig] = load_config()
+    if config is None:
+        print(f"Missing config file")
+        exit(1)
     
-    if action == "init-db":
-        initialize_db()
-        return
-    
-    if action == "create-user":
-        create_user(args)
-        return
+    # create log directory, ok if the log directory is already there
+    os.makedirs(config.core.log_dir, exist_ok=True)
 
-    # if action == "test":
-    #     test(args)
-    #     return
-
-def initialize_db():
-    config = load_config()
-    db_engine = create_engine(config.core.db_url)
-    create_all_tables(db_engine)
-
-def create_user(args):
-    password1 = getpass.getpass("Enter your password: ")
-    password2 = getpass.getpass("Enter your password again: ")
-    if password1 != password2:
-        print("Mismatched password")
+    # loading logging config
+    if not os.path.isfile(config.core.log_config_filename):
+        print(f"Missing log config file: {config.core.log_config_filename}")
         exit(1)
 
-    config = load_config()
-    db_engine = create_engine(config.core.db_url)
-    um = UserManager(
-        db_engine=db_engine, 
-        private_key=config.core.private_key, 
-        public_key=config.core.public_key
-    )
-    user = um.create_user(email=args.email, password=password1)
-    if user is None:
-        print(f"Unable to create user")
-        exit(1)
+    with open(config.core.log_config_filename, "rt") as f:
+        log_config = yaml.safe_load(f)
 
-    print(f"User created, id={user.id}, email={user.email}")
+    # patch the log config
+    for _, handler_config in log_config.get("handlers", {}).items():
+        if "filename" in handler_config:
+            handler_config["filename"] = normalize_filename(
+                config.core.log_dir, 
+                handler_config["filename"]
+            )
+    logging.config.dictConfig(log_config)
+    logger = logging.getLogger(__name__)
+    logger.info("Logging is initialized")
 
-# def test(args):
-#     jwt_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0b25lemhvbmdAaG90bWFpbC5jb20iLCJwYXNzd29yZF92ZXJzaW9uIjoxLCJzdWIiOiIxIiwidXVpZCI6ImFmNmM4Yjc1LTM5ZjctNGVhMS04NjczLTgyYWI1ZTA5YTdjZSJ9.Aea_3-oxSBHHVXFS9ZB0jlo6qKKQDkDWzf_0o0exxEE6HUxLn7oRJSm5m2JVVeJy6cueXBbEAOY7urLw-HaHo4oCFaKIxPHUVurto99ITw84WSUgxQh58uN4H-Qt1-XiZTqALtu1CFL5ZfFXQMEX0KvxXLJ11nSr2c9uAzO6WmznAVXrKymvzTcae3Q992inPjaj9AJtFpPY5WZ_BH7iZ6MaQNogyFLFq8zuQMTSxm0WIgCBAbZmsUN1Brrwrgt7OY-I-lTQAgzmxMddyi-JtcKYH9jIAyQ8klpaMfM4u8K7Wxl4js8Bd-0JWjVxyKFwEwdDo32sMC9Jg-qql4D7_Q"
-#     config = load_config()
-#     db_engine = create_engine(config.core.db_url)
-#     um = UserManager(
-#         db_engine=db_engine, 
-#         private_key=config.core.private_key, 
-#         public_key=config.core.public_key
-#     )
-#     t = um.extract_payload_from_jwt_token(jwt_token)
-#     print(t)
-
-# def test(args):
-#     from sqlalchemy import Engine, select, func
-#     from sqlalchemy.orm import Session
-#     from webcli2.db_models import DBAction, DBActionHandlerConfiguration, DBUser, DBThread, DBThreadAction
-#     config = load_config()
-#     db_engine = create_engine(config.core.db_url)
-#     with Session(db_engine) as session:
-#         with session.begin():
-#             q = select(
-#                 func.max(DBThreadAction.display_order)
-#             ).where(DBThreadAction.thread_id == 11)
-
-#             v = session.scalars(q).one()
-#             print(v)
-
-
-
-
+    from .main_internal import webcli_internal
+    webcli_internal(config, log_config)
---
+++ src/webcli2/cli/main_internal.py
@@ -0,0 +1,115 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import argparse
+import getpass
+
+from sqlalchemy import create_engine
+
+from webcli2.config import WebCLIApplicationConfig, load_config, normalize_filename
+from webcli2.webcli_engine import UserManager
+from webcli2.db_models import create_all_tables
+
+class WebCLIApplication:
+    config:WebCLIApplicationConfig      # config loaded from webcli_cfg.yaml
+    
+    def __init__(self, config:WebCLIApplicationConfig):
+        self.config = config
+
+    def start(self, log_config:dict, args:argparse.Namespace):
+        import uvicorn
+        from webcli2.web import app
+        # run application
+        uvicorn.run(app, host=args.host, port=args.port, reload=False, log_config=log_config)
+
+def webcli_internal(config:WebCLIApplicationConfig, log_config:dict):
+    parser = argparse.ArgumentParser(
+        description='WebCLI Tool.'
+    )
+    parser.add_argument(
+        "action", type=str, help="Specify action",
+        choices=['start', 'init-db', 'create-user'],
+        nargs=1
+    )
+    parser.add_argument(
+        "--port", type=int, required=False, default=8000, help="Web Server binding port number"
+    )
+    parser.add_argument(
+        "--host", type=str, required=False, default="127.0.0.1", help="Web Server binding IP address"
+    )
+    parser.add_argument(
+        "--email", type=str, required=False, help="user email"
+    )
+    args = parser.parse_args()
+    action = args.action[0]
+    if action == "start":
+        webcli_application = WebCLIApplication(config)
+        webcli_application.start(log_config, args)
+        return
+    
+    if action == "init-db":
+        initialize_db(config)
+        return
+    
+    if action == "create-user":
+        create_user(config, args)
+        return
+
+    # if action == "test":
+    #     test(args)
+    #     return
+
+def initialize_db(config:WebCLIApplicationConfig):
+    db_engine = create_engine(config.core.db_url)
+    create_all_tables(db_engine)
+
+def create_user(config:WebCLIApplicationConfig, args:argparse.Namespace):
+    password1 = getpass.getpass("Enter your password: ")
+    password2 = getpass.getpass("Enter your password again: ")
+    if password1 != password2:
+        print("Mismatched password")
+        exit(1)
+
+    db_engine = create_engine(config.core.db_url)
+    um = UserManager(
+        db_engine=db_engine, 
+        private_key=config.core.private_key, 
+        public_key=config.core.public_key
+    )
+    user = um.create_user(email=args.email, password=password1)
+    if user is None:
+        print(f"Unable to create user")
+        exit(1)
+
+    print(f"User created, id={user.id}, email={user.email}")
+
+# def test(args):
+#     jwt_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0b25lemhvbmdAaG90bWFpbC5jb20iLCJwYXNzd29yZF92ZXJzaW9uIjoxLCJzdWIiOiIxIiwidXVpZCI6ImFmNmM4Yjc1LTM5ZjctNGVhMS04NjczLTgyYWI1ZTA5YTdjZSJ9.Aea_3-oxSBHHVXFS9ZB0jlo6qKKQDkDWzf_0o0exxEE6HUxLn7oRJSm5m2JVVeJy6cueXBbEAOY7urLw-HaHo4oCFaKIxPHUVurto99ITw84WSUgxQh58uN4H-Qt1-XiZTqALtu1CFL5ZfFXQMEX0KvxXLJ11nSr2c9uAzO6WmznAVXrKymvzTcae3Q992inPjaj9AJtFpPY5WZ_BH7iZ6MaQNogyFLFq8zuQMTSxm0WIgCBAbZmsUN1Brrwrgt7OY-I-lTQAgzmxMddyi-JtcKYH9jIAyQ8klpaMfM4u8K7Wxl4js8Bd-0JWjVxyKFwEwdDo32sMC9Jg-qql4D7_Q"
+#     config = load_config()
+#     db_engine = create_engine(config.core.db_url)
+#     um = UserManager(
+#         db_engine=db_engine, 
+#         private_key=config.core.private_key, 
+#         public_key=config.core.public_key
+#     )
+#     t = um.extract_payload_from_jwt_token(jwt_token)
+#     print(t)
+
+# def test(args):
+#     from sqlalchemy import Engine, select, func
+#     from sqlalchemy.orm import Session
+#     from webcli2.db_models import DBAction, DBActionHandlerConfiguration, DBUser, DBThread, DBThreadAction
+#     config = load_config()
+#     db_engine = create_engine(config.core.db_url)
+#     with Session(db_engine) as session:
+#         with session.begin():
+#             q = select(
+#                 func.max(DBThreadAction.display_order)
+#             ).where(DBThreadAction.thread_id == 11)
+
+#             v = session.scalars(q).one()
+#             print(v)
+
+
+
+
